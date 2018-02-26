200a4e3c35fc75d0b4fef4ce434be5ef58b991d7
Mingkai Ma <mamingkai824@gmail.com>
2018-02-26 21:44:47 +1100

    added

---
+++ test_sizeof.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int data1[] = {1, 2, 3, 4, 5};
+    cout << sizeof(data1) << "\n";
+
+    int* d = data1;
+    cout << sizeof(data1) << "\n";
+
+
+}
