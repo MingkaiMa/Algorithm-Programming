7212fc9ccaf6e431871a158ae23a2787da60577c
Mingkai Ma <mamingkai824@gmail.com>
2018-03-02 14:05:51 +1100

    verify whether a sequence is one BST's postorder

---
+++ VerifyBSTPostorder.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+
+struct BinaryTreeNode
+{
+    int value;
+    BinaryTreeNode* left;
+    BinaryTreeNode* right;
+};
+
+
+
+bool verify(int sequence[], int length)
+{
+    if(sequence == NULL || length <= 0)
+        return false;
+
+
+    int root sequence[length - 1];
+
+
+    int i = 0;
+    for(; i < length - 1; i++)
+    {
+        if(sequence[i] > root)
+        {
+            break;
+        }
+    }
+
+    int j = i;
+
+    for(; j < length - 1; j++)
+    {
+        if(sequence[j] < root)
+            return false;
+    }
+
+
+    bool left = true;
+    if(i > 0)
+        left = verify(sequence, i);
+
+    bool right = true;
+    if(i < length - 1)
+        right = verify(sequence + i, length - i - 1);
+
+
+    return (left && right);
+        
+}
