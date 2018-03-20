cae51465453182f0030f575bb861f4ff8e52f9cf
Mingkai Ma <mamingkai824@gmail.com>
2018-03-21 01:47:03 +1100

    sqrt 66

---
+++ Sqrt.cc
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        
+        if(x <= 1)
+            return x;
+        
+        double pre = 0;
+        double cur = x;
+        
+        while(abs(cur - pre) > 0.000001)
+        {
+            pre = cur;
+            cur = (pre / 2 + x / (2 * pre));
+        }
+                
+        return int(cur);
+    }
+};
