0bbb3a071ea7124362868ddf927e181d0a5b252e
Mingkai Ma <mamingkai824@gmail.com>
2018-05-12 01:07:22 +1000

    327. Count of Range Sum

---
+++ CountOfRangeSum.cc
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int mergeCore(vector<long>& sum, int lower, int upper, int low, int high) {
+        if(high - low <= 1)
+            return 0;
+        
+        int mid = low + (high - low) / 2;
+        int m = mid;
+        int n = mid;
+        int count = 0;
+        
+        count = mergeCore(sum, lower, upper, low, mid) + mergeCore(sum, lower, upper, mid, high);
+        
+        for(int i = low; i < mid; i++) {
+            while(m < high && sum[m] - sum[i] < lower)
+                m++;
+            while(n < high && sum[n] - sum[i] <= upper)
+                n++;
+            
+            count += n - m;
+        }
+        inplace_merge(sum.begin() + low, sum.begin() + mid, sum.begin() + high);
+        return count;
+        
+        
+        
+    }
+    int countRangeSum(vector<int>& nums, int lower, int upper) {
+        int len = nums.size();
+        vector<long> sum(len + 1, 0);
+        for(int i = 0; i < len; i++)
+            sum[i + 1] = sum[i] + nums[i];
+        
+        return mergeCore(sum, lower, upper, 0, len + 1);
+    }
+};
